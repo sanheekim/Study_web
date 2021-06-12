84170444a0cbdcfe8cd7915a78ebed996f54e8a4
sanheekim <68312957+sanheekim@users.noreply.github.com>
2021-06-12 22:27:59 +0900

    do it 자바스크립트 + 제이쿼리 입문_html 자동완성 : !+[Tab]

---
+++ pro06/src/pro06/sec01/ex01/InputServlet.java
@@ -0,0 +1,33 @@
+package pro06.sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+@WebServlet("/InputServlet")
+public class InputServlet extends HttpServlet {
+       
+	public void init() throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
