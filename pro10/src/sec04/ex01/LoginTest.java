4c1d40550baaeb89299dfd7f440f0742d61bb487
sanheekim <68312957+sanheekim@users.noreply.github.com>
2021-06-23 14:31:54 +0900

    자바 웹을 다루는 기술_서블릿의 필터와 리스너 기능 (4)
    - 여러가지 서블릿 관련 Listener API

--- pro10/src/sec04/ex01/LoginTest.java
+++ pro10/src/sec04/ex01/LoginTest.java
@@ -1,4 +1,4 @@
-package sec03.ex01;
+package sec04.ex01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginTest
@@ -19,14 +20,21 @@ public class LoginTest extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// request.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		String user_name = request.getParameter("user_name");
+		HttpSession session = request.getSession();
+		String user_id = request.getParameter("user_id");
 		String user_pw = request.getParameter("user_pw");
-		out.println("<html><body>");
-		out.println("이름은 "+user_name+"<br>");
-		out.println("비밀번호는 "+user_pw+"<br>");
+		LoginImpl loginUser = new LoginImpl(user_id, user_pw);
+		if (session.isNew()) {
+			session.setAttribute("loginUser", loginUser);
+		}
+		out.println("<html><script type='text/javascript'>");
+		out.println("setTimeout(listory.go(0);',5000");
+		out.println("</script></head><html><body>");
+		out.println("아이디는 "+loginUser.user_id+"<br>");
+		out.println("총 접속자수는 "+LoginImpl.total_user+"<br>");
 		out.println("</body></html>");
 	}
 
