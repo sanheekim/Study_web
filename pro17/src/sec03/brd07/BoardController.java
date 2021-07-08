e00095c435a7840ce93ff891ca966c52a970d630
sanheekim <68312957+sanheekim@users.noreply.github.com>
2021-07-08 14:01:36 +0900

    자바 웹을 다루는 기술_모델2 방식으로 효율적으로 개발하기 (5)
    - 모델2로 답변형 게시판 구현하기 (글 삭제) : 실패

--- pro17/src/sec03/brd07/BoardController.java
+++ pro17/src/sec03/brd07/BoardController.java
@@ -1,4 +1,4 @@
-package sec03.brd05;
+package sec03.brd07;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,9 +26,9 @@ import org.apache.commons.io.FileUtils;
  * Servlet implementation class BoardController
  */
 
-//@WebServlet("/board/*")
+@WebServlet("/board/*")
 public class BoardController extends HttpServlet {
-	private static String ARTICLE_IMAGE_REPO = "C:\\board\\article_image";
+	private static String ARTICLE_IMAGE_REPO = "C:\\KHDev\\Workspace_Web_bymyself\\board\\article_image";
 	BoardService boardService;
 	ArticleVO articleVO;
 
@@ -67,13 +67,13 @@ public class BoardController extends HttpServlet {
 			if (action == null) {
 				articlesList = boardService.listArticles();
 				request.setAttribute("articlesList", articlesList);
-				nextPage = "/board04/listArticles.jsp";
+				nextPage = "/board05/listArticles.jsp";
 			} else if (action.equals("/listArticles.do")) {
 				articlesList = boardService.listArticles();
 				request.setAttribute("articlesList", articlesList);
-				nextPage = "/board04/listArticles.jsp";
+				nextPage = "/board05/listArticles.jsp";
 			} else if (action.equals("/articleForm.do")) {
-				nextPage = "/board04/articleForm.jsp";
+				nextPage = "/board05/articleForm.jsp";
 			} else if (action.equals("/addArticle.do")) {
 				int articleNO = 0;
 				Map<String, String> articleMap = upload(request, response);
@@ -92,7 +92,6 @@ public class BoardController extends HttpServlet {
 					File destDir = new File(ARTICLE_IMAGE_REPO + "\\" + articleNO);
 					destDir.mkdirs();
 					FileUtils.moveFileToDirectory(srcFile, destDir, true);
-					srcFile.delete();
 				}
 				PrintWriter pw = response.getWriter();
 				pw.print("<script>" + "  alert('새글을 추가했습니다.');" + " location.href='" + request.getContextPath()
@@ -103,7 +102,7 @@ public class BoardController extends HttpServlet {
 				String articleNO = request.getParameter("articleNO");
 				articleVO = boardService.viewArticle(Integer.parseInt(articleNO));
 				request.setAttribute("article", articleVO);
-				nextPage = "/board04/viewArticle.jsp";
+				nextPage = "/board05/viewArticle.jsp";
 			} else if (action.equals("/modArticle.do")) {
 				Map<String, String> articleMap = upload(request, response);
 				int articleNO = Integer.parseInt(articleMap.get("articleNO"));
@@ -131,9 +130,23 @@ public class BoardController extends HttpServlet {
 				pw.print("<script>" + "  alert('글을 수정했습니다.');" + " location.href='" + request.getContextPath()
 						+ "/board/viewArticle.do?articleNO=" + articleNO + "';" + "</script>");
 				return;
+			} else if (action.equals("/removeArticle.do")) {
+				int articleNO = Integer.parseInt(request.getParameter("articleNO"));
+				List<Integer> articleNOList = boardService.removeArticle(articleNO);
+				for (int _articleNO : articleNOList) {
+					File imgDir = new File(ARTICLE_IMAGE_REPO + "\\" + _articleNO);
+					if (imgDir.exists()) {
+						FileUtils.deleteDirectory(imgDir);
+					}
+				}
+
+				PrintWriter pw = response.getWriter();
+				pw.print("<script>" + "  alert('글을 삭제했습니다.');" + " location.href='" + request.getContextPath()
+						+ "/board/listArticles.do';" + "</script>");
+				return;
 			
 			}else {
-				nextPage = "/board04/listArticles.jsp";
+				nextPage = "/board05/listArticles.jsp";
 			}
 
 			RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
