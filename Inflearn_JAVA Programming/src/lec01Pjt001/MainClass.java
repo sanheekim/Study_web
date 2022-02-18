ffd1496fb8d392d66bbc326cebf699016952717a
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-02-18 19:13:21 +0900

    inflearn 자바 프로그래밍 입문 강좌 (renew ver.) 1~3강

---
+++ Inflearn_JAVA Programming/src/lec01Pjt001/MainClass.java
@@ -0,0 +1,11 @@
+package lec01Pjt001;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, Java World! \nNice to meet you again");
+		// F11 누르면 실행
+		// java programming을 하려면 jdk ∋ jre ∋ api ∋ jvm 을 사용해야 함.
+	}
+
+}
