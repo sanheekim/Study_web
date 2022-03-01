47f27122d2be2b43f60c7975c22d9ecc7f221953
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-03-02 08:33:05 +0900

    inflearn 자바 프로그래밍 입문 강좌 (renew ver.) 25~26강

--- Inflearn_JAVA Programming/src/lec02Pjt001/MainClass.java
+++ Inflearn_JAVA Programming/src/lec02Pjt001/MainClass.java
@@ -1,5 +1,8 @@
 package lec02Pjt001;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainClass {
 	public static void main(String[] args) {
 		
@@ -67,7 +70,57 @@ public class MainClass {
 		};
 		System.out.printf("li6.method(10,20):%d\n", li6.method(20, 10));
 
-	
+		// String 클래스 : String은 데이터가 변화하면 기존의 객체를 버리고 새로운 객체를 메모리에 생성해서 메모리를 많이 잡아먹음.
+		// (기존 객체는 Garbage Collector에 의해서 메모리 회수가 이루어짐.)
+		String str = "JAVA";
+		str = str + "_8";
+		
+		// StringBuffer, StringBuilder 클래스 : String 클래스의 단점을 보완한 클래스로, 데이터가 변경되면 '메모리에서 기존 객체를 재활용'함.
+		// (둘은 거의 똑같은 클래스. Buffer가 나온 후 Builder가 나옴. 데이터안정성은 Buffer가 조금 더 좋고, 속도는 Builder가 더 빠름.)
+		StringBuffer sf = new StringBuffer("JAVA");
+		sf.append("_8");
+		
+		// ***** Collections - List [], Map {} *****
+		// List : 인터페이스임. 이를 구현한 클래스는 '인덱스'를 이용해서 데이터를 관리함. 데이터 중복이 가능함. ex) Vector, ArrayList, LinkedList
+		ArrayList<String> list = new ArrayList<String>();
+		// 데이터 추가
+		list.add("Hello");
+		list.add("Java");
+		list.add("World");
+		list.add(2,"Programing"); // 추가 // Hello Java Programing World
+		list.set(1, "C"); // 변경 // Hello C Programing World
+		// 데이터 추출
+		String strList = list.get(2);
+		System.out.println(strList); // Programing
+		// 데이터 제거
+		strList = list.remove(2); // Hello C World
+		// 데이터 저네 제거
+		list.clear();
+		// 데이터 유무
+		boolean b = list.isEmpty();
+		System.out.println(b); // true
+		
+		// Map : 인터페이스임. 이를 구현한 클래스는 'Key'를 이용해서 데이터 관리함. key는 중복 불가능. 데이터 중복 가능. ex) HashMap
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		// 데이터 추가
+		map.put(5, "Hello"); // key값이 5일 때 "Hello" 넣기
+		map.put(6, "Java");
+		map.put(7, "World");
+		map.put(8, "!");
+		// 데이터 교체
+		map.put(6, "C");
+		// 데이터 추출
+		String strMap = map.get(5);
+		System.out.println(strMap); // Hello
+		// 데이터 제거
+		map.remove(8); // {5=Hello, 6=C, 7=World}
+		// 특정데이터 포함 유무
+		b = map.containsKey(7); // true
+		b = map.containsValue("World"); // true
+		// 데이터 전체 제거
+		map.clear(); // {}
+		// 데이터 유무
+		b = map.isEmpty(); // true
 	}
 
 }
