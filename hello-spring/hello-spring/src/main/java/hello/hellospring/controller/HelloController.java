85f9bce932ac53d9aeddc9e212d17570c7a45349
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-03-07 18:33:32 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 8강

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,9 +3,14 @@ package hello.hellospring.controller;
 @Controller
 public class HelloController {
 
-	@GetMapping("hello")
-	public String hello(Model model) {
-		model.addAttribute(attributeName:"data",attributeValue:"hello!");
-		return "hello";
+	 @GetMapping("hello") public String hello(Model model) {
+	 model.addAttribute(attributeName:"data",attributeValue:"hello!");
+	 return "hello"; }
+	
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam("name") String name, Model model) {
+		model.addAttribute(attributeName:"name",name);
+		return "hello-template";
 	}
+	
 }
