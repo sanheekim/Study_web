d2b1835dba0c11c04d70edd32095c9c7f074ff85
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-03-08 11:05:26 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 9강

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -7,10 +7,37 @@ public class HelloController {
 	 model.addAttribute(attributeName:"data",attributeValue:"hello!");
 	 return "hello"; }
 	
+	// MVC
 	@GetMapping("hello-mvc")
 	public String helloMvc(@RequestParam("name") String name, Model model) {
 		model.addAttribute(attributeName:"name",name);
 		return "hello-template";
 	}
 	
+	// API
+	@GetMapping("hello-string")
+	@ResponseBody // http에서 header/body 중 body부에 아래 return 데이터를 '직접' 넣어주겠다는 의미.
+	public String helloString(@RequestParam("name") String name) {
+		return "hello " + name;
+	}
+	
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		return hello;
+	}
+	 
+	static class Hello {
+		private String name;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
 }
