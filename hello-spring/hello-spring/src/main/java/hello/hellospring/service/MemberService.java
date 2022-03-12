cc3cd581bb34f4a6efceab632b419e30487db0bb
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-03-12 22:13:12 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 13강

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,51 @@
+package hello.hellospring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+public class MemberService {
+	
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	
+	/**
+	 * 회원 가입
+	 * @param member
+	 * @return
+	 */
+	public Long join(Member member) {
+		validateDuplicateMember(member); //중복 회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+	
+	private void validateDuplicateMember(Member member) {
+		// '같은 이름이 있는 중복 회원 X'라는 비즈니스 로직
+		// 방법 1
+		/* 
+		 * Optional<Member> result = memberRepository.findByName(member.getName());
+		 * result.ifPresent(m -> { throw new IllegalStateException("이미 존재하는 회원입니다.");
+		 * });
+		 * 
+		 */
+		// 방법 2
+		 memberRepository.findByName(member.getName()) .ifPresent(m -> { throw new
+		 IllegalStateException("이미 존재하는 회원입니다.");
+		 });
+	}
+	
+	/**
+	 * 전체 회원 조회
+	 */
+	public List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	public Optional<Member> findOne(Long memberId){
+		return memberRepository.findById(memberId);
+	}
+	
+}
